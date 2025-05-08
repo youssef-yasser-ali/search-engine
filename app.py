e269cefa87f40b8dd7e2fd4cbfe16e6364e080f5
youssef-yasser-ali <yyasser849@gmail.com>
2025-05-08 03:10:51 +0300

    adding data

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, request, render_template, jsonify
+
 from utils import search_word
 
+
 app = Flask(__name__)
+
+
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/search')
-def api_search():
-    if not (query := request.args.get('q', '').strip()):
-        return jsonify({"error": "Missing query"}), 400
-    return jsonify({
-        "query": query,
-        "results": search_word(query),
-        "count": len(search_word(query))
-    })
+# @app.route('/api/search')
+# def api_search():
+#     if not (query := request.args.get('q', '').strip()):
+#         return jsonify({"error": "Missing query"}), 400
+#     return jsonify({
+#         "query": query,
+#         "results": search_word(query),
+#         "count": len(search_word(query))
+#     })
 
 @app.route('/search')
 def web_search():
